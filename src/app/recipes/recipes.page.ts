bf64ff1e89cc3a9ed4a38ebe141dacef6831c9d7
André Lashley <andre.lashley@gmail.com>
2020-05-10 21:26:04 -0700

    Add lifecycle hooks to recipes page

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RecipesService } from './recipes.service';
 import { Recipe } from './recipe.model';
 
@@ -7,13 +7,32 @@ import { Recipe } from './recipe.model';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
   recipes: Recipe[];
 
   constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillEnter() {
     this.recipes = this.recipesService.getAllRecipes();
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
   }
 
 }
